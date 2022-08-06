e4c16bf1e39dafb55d572cb818aeced2f49d6599
Valkuros <Valkuros@gmail.com>
2022-08-06 12:17:51 -0400

    First Conditional Example

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-s
+const btn = document.querySelector('button');
+const txt = document.querySelector('p');
+
+btn.addEventListener('click', updateBtn);
+
+function updateBtn() {
+    if (btn.textContent === 'Start Machine') {
+        btn.text = 'Stop Machine';
+    text.textContent = 'The machine is running!';
+    } else {
+        btn.textContent = 'Start Machine';
+        txt.textContent = 'The machine is stopped.';
+    }
+}
